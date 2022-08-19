2583585050bda2f41cee282c7021f734a7b2c0d4
Federico Blautzik <blautzik.f@gmail.com>
2022-08-19 17:48:05 -0300

    tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '../styles/globals.css'
-import NavB from './Components/NavB/NavB';
+
 import Nav from './Components/NavBa/NavBa'
 
 function MyApp({ Component, pageProps }) {
   return (
   <>
-    {/* <NavB /> */}
+
     <Nav className='drop-shadow-md' />
     <Component {...pageProps} />
   </>
